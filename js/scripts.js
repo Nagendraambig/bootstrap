e46d8276abd75a57f3952ec9309853401731f081
nagendraambig <nagendraambig79@gmail.com>
2020-04-18 16:59:22 +0530

    End

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $("#mymodal").click(function(){
+    $("#reservemodal").modal();
+});
+    $("a span").click(function(){
+    $("#loginModal").modal();  
+});
+});
